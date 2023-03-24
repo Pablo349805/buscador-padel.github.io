1cbfc4c7d556474cce49922f13526665c8e78a41
Pablo349805 <4984519gmail.com>
2023-03-24 19:04:21 -0300

    agregamos archivos

---
+++ src/components/Contenido.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/Contenido.css"
+
+const Contenido = () => {
+  return (
+    <div>
+        <h1>¿Busca un pareja de padel? ¡Regístrate ahora!</h1>
+        <h2>El sitio web de pareja de padel en Argentina</h2>
+
+        <button className='btnContenido'>Encuentra pareja de padel</button>
+        
+        
+        </div>
+  )
+}
+
+export default Contenido
